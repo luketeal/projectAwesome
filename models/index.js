1465ef3bd586fc802ae6c20259f0be6972434662
Luke Teal <l.teal21@gmail.com>
2021-06-15 19:18:09 -0400

    add table relationships

--- models/index.js
+++ models/index.js
@@ -2,5 +2,29 @@ const User = require('./Users');
 const Race = require('./Races');
 const Car = require('./Cars');
 
+User.hasMany(Car, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+  
+Car.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Race, Comment };
+Race.belongsToMany(Car, {
+    through: 'event',
+});
+
+Car.belongsToMany(Race, {
+    through: 'event'
+});
+
+Race.belongsToMany(User, {
+    through: 'event',
+});
+
+User.belongsToMany(Race, {
+    through: 'event'
+});
+
+module.exports = { User, Race, Car };
