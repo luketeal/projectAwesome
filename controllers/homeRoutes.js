27336aede74eea1c8c974422394bc4a736b83678
Luke Teal <l.teal21@gmail.com>
2021-06-16 09:45:56 -0400

    created homeroutes to render the handlebars pages I know there is no data being passed to the handlebars pages yet. I'll jump on seeding the database next so there is something accessible.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,14 +3,33 @@ const sequelize = require('../config/connection');
 const { Cars, Races, Users } = require('../models');
 
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    // if (req.session.logged_in) {
-    //   res.redirect('/');
-    //   return;
-    // }
   
     res.render('login');
 });
 
+router.get('/logout', (req, res) => {
+  
+    res.render('logout');
+});
+
+router.get('/signup', (req, res) => {
+  
+    res.render('signup');
+});
+
+router.get('/', (req, res) => {
+  
+    res.render('homepage');
+});
+
+router.get('/garage', (req, res) => {
+  
+    res.render('garage');
+});
+
+router.get('/race', (req, res) => {
+  
+    res.render('race');
+});
 
 module.exports = router;
