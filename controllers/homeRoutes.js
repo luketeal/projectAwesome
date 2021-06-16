924c61085e721c0cf857a02215344de483deb028
Luke Teal <l.teal21@gmail.com>
2021-06-15 21:07:41 -0400

    testing routes and html

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Cars, Races, Users } = require('../models');
 
-router.get('/login', async (req, res) => {
-
-
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //   res.redirect('/');
+    //   return;
+    // }
+  
     res.render('login');
 });
+
+
+module.exports = router;
