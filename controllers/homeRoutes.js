411f8afcc7f2f8f90003a997ed23a23215b548ca
Luke Teal <l.teal21@gmail.com>
2021-06-16 23:28:58 -0400

    add basic get route for homepage add examples of the information available in the homepage handlebars so you can use the information format it

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Cars, Races, Users } = require('../models');
+const { Car, Race, User, Entry } = require('../models');
 
 router.get('/login', async (req, res) => {
   
@@ -18,8 +18,20 @@ router.get('/signup', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-  
-    res.render('homepage');
+  try 
+  {
+    // Get all Race data
+    const raceData = await Race.findAll();
+    const races = raceData.map((race) => race.get({ plain: true }));
+    console.log(races)
+    res.render('homepage', {
+        races, 
+        logged_in: req.session.logged_in
+    });
+  } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
+    }
 });
 
 router.get('/garage', async (req, res) => {
