97299f7b2c136759d461cce41b3b557ce19d9c20
Corey Nance <coreynance83@gmail.com>
2021-06-19 12:37:45 -0400

    made revisions to the entry file as well as the index.js files

---
+++ controllers/api/entryRoutes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const Entry = require('../../models/Entry'); 
+
+
+
+
+// http://localhost:3001/api/entries
+router.post('/', async (req, res) => {
+    const entryData = await Entry.create(req.body);
+  
+    return res.json(entryData);
+  });
+
+
+
+// // http://localhost:3001/api/entries/id
+router.put('/:id', async (req, res) => {    // Need to find out path to where we will be entering car info.
+
+    const entryData = await Entry.findByPk(req.params.id)
+
+    if(entryData) {
+        entryData.race_id = req.body.race_id;
+        entryData.user_id = req.body.user_id;
+        entryData.car_id = req.body.car_id;
+        await entryData.save()
+    }
+    else {
+        res.status(404).send()
+    }
+    return res.json(entryData);
+})
+
+
+// http://localhost:3001/api/entries/id
+router.delete('/:id', async (req, res) => {
+    const entryData = await Entry.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+  
+    return res.json(entryData);
+  });
+  
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const cars = require('./carRoutes');
+const entries = require('./entryRoutes');
 
 router.use('/cars', cars);
+router.use('/entries', entries);
 
 module.exports = router;
